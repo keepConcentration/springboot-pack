0f5f9c6d5adc45377a21081e114e0f8661955c9f
keepConcentration <phm543@gmail.com>
2022-10-05 16:47:16 +0900

    elk 환경 구축 중

---
+++ springboot-elk/src/test/java/com/teamknp/elk/SpringbootElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamknp.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
