963285eb2c71e460ff35fa2309210cc466647351
keepConcentration <phm543@gmail.com>
2022-10-17 07:51:22 +0900

    log 설정 추가

--- springboot-elk/src/main/java/com/teamknp/elk/controller/ElkController.java
+++ springboot-elk/src/main/java/com/teamknp/elk/controller/ElkController.java
@@ -1,12 +1,8 @@
 package com.teamknp.elk.controller;
 
-import com.teamknp.elk.domain.HelloDto;
-import com.teamknp.elk.domain.ResponseDto;
-import com.teamknp.elk.repository.Hello;
-import com.teamknp.elk.service.ElkService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.joda.time.LocalDate;
+import net.logstash.logback.encoder.org.apache.commons.lang.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +13,32 @@ import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/elk")
 @RequiredArgsConstructor
 public class ElkController {
 
-    private final RestTemplate restTemplate;
+    // private final RestTemplate restTemplate;
 
-    private final ElkService elkService;
-
-    @GetMapping(value = "elk")
+    @GetMapping(value = "/hello")
     @ResponseBody
     public ResponseEntity<?> hello() {
         String response = "Hello Elk! " + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE);
+        log.info(response);
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/exception")
+    @ResponseBody
+    public ResponseEntity<?> exception() {
+        String response = "";
+        try {
+            throw new Exception("Exception has occured.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            String stackTrace = ExceptionUtils.getStackTrace(e);
+            log.error("Exception: " + stackTrace);
+            response = stackTrace;
+        }
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
