0be416e2f58f4d0e1ecdf29aac3e398c288f3c18
keepConcentration <phm543@gmail.com>
2022-10-17 08:48:49 +0900

    Spring Batch 최초 커밋

---
+++ springboot-batch/src/main/java/com/phm/springbootbatch/step/Reader.java
@@ -0,0 +1,29 @@
+package com.phm.springbootbatch.step;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class Reader implements ItemReader<String> {
+
+    private String[] messages = { "hello",
+            "Batch!",
+            "World!" };
+
+    private int count = 0;
+
+    @Override
+    public String read() throws Exception, UnexpectedInputException,
+            ParseException, NonTransientResourceException {
+
+        if (count < messages.length) {
+            return messages[count++];
+        } else {
+            count = 0;
+        }
+
+        return null;
+    }
+
+}
